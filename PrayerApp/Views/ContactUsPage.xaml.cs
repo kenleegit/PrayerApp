ce785735fede2737026750c365e65a17fdab666f
kenelegit <ahkenlee@yahoo.com.hk>
2018-01-30 23:40:11 +0800

    Added a draft ContactUsPage

---
+++ PrayerApp/Views/ContactUsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PrayerApp
+{
+    public partial class ContactUsPage : ContentPage
+    {
+        public ContactUsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
